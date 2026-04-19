fd34f20bfe6b0c3b27a078fb892e6e39b3d7a335
Marwan Tamer <tamermarwan054@gmail.com>
2026-04-19 18:44:20 +0200

    added joystick_private.h

---
+++ HAL/joystick_private.h
@@ -0,0 +1,12 @@
+#ifndef JOYSTICK_PRIVATE_H
+#define JOYSTICK_PRIVATE_H
+
+#include "Types.h"
+
+// Thresholds with dead zone (adjust if needed)
+#define JOY_THRESHOLD_HIGH   700
+#define JOY_THRESHOLD_LOW    300
+#define JOY_CENTER_MIN       450
+#define JOY_CENTER_MAX       550
+
+#endif /* JOYSTICK_PRIVATE_H */
